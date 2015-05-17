98e873656783a5b71e05319d9a5cb39089525e33
Roman <roman34rus@inbox.ru>
2015-05-17 15:40:02 +0300

    Version 2.0.

--- KeyPressEmulator/FormMain.Designer.cs
+++ KeyPressEmulator/FormMain.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.Timer = new System.Windows.Forms.Timer(this.components);
             this.Status = new System.Windows.Forms.StatusStrip();
+            this.StatusEmuState = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusPaused = new System.Windows.Forms.ToolStripStatusLabel();
             this.StatusCopyright = new System.Windows.Forms.ToolStripStatusLabel();
             this.BtnStop = new System.Windows.Forms.Button();
             this.BtnStart = new System.Windows.Forms.Button();
@@ -38,6 +40,10 @@
             this.LabelFreq = new System.Windows.Forms.Label();
             this.TextKey = new System.Windows.Forms.TextBox();
             this.LabelKey = new System.Windows.Forms.Label();
+            this.LabelHotKey = new System.Windows.Forms.Label();
+            this.ComboHotKey = new System.Windows.Forms.ComboBox();
+            this.BtnMapHotKey = new System.Windows.Forms.Button();
+            this.LabelHotKey2 = new System.Windows.Forms.Label();
             this.Status.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,83 +55,153 @@
             // Status
             // 
             this.Status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusEmuState,
+            this.StatusPaused,
             this.StatusCopyright});
-            this.Status.Location = new System.Drawing.Point(0, 148);
+            this.Status.Location = new System.Drawing.Point(0, 218);
             this.Status.Name = "Status";
-            this.Status.Size = new System.Drawing.Size(227, 22);
+            this.Status.Size = new System.Drawing.Size(343, 22);
             this.Status.TabIndex = 9;
             this.Status.Text = "statusStrip1";
+            // 
+            // StatusEmuState
+            // 
+            this.StatusEmuState.Name = "StatusEmuState";
+            this.StatusEmuState.Size = new System.Drawing.Size(135, 17);
+            this.StatusEmuState.Text = "Эмуляция остановлена";
+            // 
+            // StatusPaused
+            // 
+            this.StatusPaused.Name = "StatusPaused";
+            this.StatusPaused.Size = new System.Drawing.Size(39, 17);
+            this.StatusPaused.Text = "Пауза";
             // 
             // StatusCopyright
             // 
             this.StatusCopyright.Name = "StatusCopyright";
             this.StatusCopyright.Size = new System.Drawing.Size(114, 17);
             this.StatusCopyright.Text = "© Любимов Роман";
+            this.StatusCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // BtnStop
             // 
             this.BtnStop.Enabled = false;
-            this.BtnStop.Location = new System.Drawing.Point(107, 112);
+            this.BtnStop.Location = new System.Drawing.Point(106, 93);
             this.BtnStop.Name = "BtnStop";
             this.BtnStop.Size = new System.Drawing.Size(75, 23);
-            this.BtnStop.TabIndex = 4;
+            this.BtnStop.TabIndex = 23;
             this.BtnStop.Text = "Стоп";
             this.BtnStop.UseVisualStyleBackColor = true;
             this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
             // 
             // BtnStart
             // 
-            this.BtnStart.Location = new System.Drawing.Point(12, 112);
+            this.BtnStart.Location = new System.Drawing.Point(11, 93);
             this.BtnStart.Name = "BtnStart";
             this.BtnStart.Size = new System.Drawing.Size(75, 23);
-            this.BtnStart.TabIndex = 3;
+            this.BtnStart.TabIndex = 22;
             this.BtnStart.Text = "Старт";
             this.BtnStart.UseVisualStyleBackColor = true;
             this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
             // TextFreq
             // 
-            this.TextFreq.Location = new System.Drawing.Point(12, 76);
+            this.TextFreq.Location = new System.Drawing.Point(12, 67);
             this.TextFreq.Name = "TextFreq";
             this.TextFreq.Size = new System.Drawing.Size(111, 20);
-            this.TextFreq.TabIndex = 2;
+            this.TextFreq.TabIndex = 21;
             this.TextFreq.Text = "200";
             // 
             // LabelFreq
             // 
             this.LabelFreq.AutoSize = true;
-            this.LabelFreq.Location = new System.Drawing.Point(12, 60);
+            this.LabelFreq.Location = new System.Drawing.Point(12, 51);
             this.LabelFreq.Name = "LabelFreq";
             this.LabelFreq.Size = new System.Drawing.Size(111, 13);
-            this.LabelFreq.TabIndex = 13;
+            this.LabelFreq.TabIndex = 25;
             this.LabelFreq.Text = "Частота повторения";
             // 
             // TextKey
             // 
             this.TextKey.AcceptsReturn = true;
             this.TextKey.AcceptsTab = true;
-            this.TextKey.Location = new System.Drawing.Point(12, 28);
+            this.TextKey.Location = new System.Drawing.Point(12, 25);
             this.TextKey.Multiline = true;
             this.TextKey.Name = "TextKey";
             this.TextKey.Size = new System.Drawing.Size(200, 20);
-            this.TextKey.TabIndex = 1;
+            this.TextKey.TabIndex = 20;
             this.TextKey.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextKey_KeyDown);
             this.TextKey.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextKey_KeyUp);
             // 
             // LabelKey
             // 
             this.LabelKey.AutoSize = true;
-            this.LabelKey.Location = new System.Drawing.Point(12, 12);
+            this.LabelKey.Location = new System.Drawing.Point(12, 9);
             this.LabelKey.Name = "LabelKey";
             this.LabelKey.Size = new System.Drawing.Size(156, 13);
-            this.LabelKey.TabIndex = 11;
+            this.LabelKey.TabIndex = 24;
             this.LabelKey.Text = "Клавиша / сочетание клавиш";
+            // 
+            // LabelHotKey
+            // 
+            this.LabelHotKey.AutoSize = true;
+            this.LabelHotKey.Location = new System.Drawing.Point(11, 161);
+            this.LabelHotKey.Name = "LabelHotKey";
+            this.LabelHotKey.Size = new System.Drawing.Size(129, 13);
+            this.LabelHotKey.TabIndex = 26;
+            this.LabelHotKey.Text = "Горячая клавиша паузы";
+            // 
+            // ComboHotKey
+            // 
+            this.ComboHotKey.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboHotKey.FormattingEnabled = true;
+            this.ComboHotKey.Items.AddRange(new object[] {
+            "F1",
+            "F2",
+            "F3",
+            "F4",
+            "F5",
+            "F6",
+            "F7",
+            "F8",
+            "F9",
+            "F10",
+            "F11",
+            "F12"});
+            this.ComboHotKey.Location = new System.Drawing.Point(11, 177);
+            this.ComboHotKey.MaxDropDownItems = 12;
+            this.ComboHotKey.Name = "ComboHotKey";
+            this.ComboHotKey.Size = new System.Drawing.Size(74, 21);
+            this.ComboHotKey.TabIndex = 27;
+            // 
+            // BtnMapHotKey
+            // 
+            this.BtnMapHotKey.Location = new System.Drawing.Point(105, 177);
+            this.BtnMapHotKey.Name = "BtnMapHotKey";
+            this.BtnMapHotKey.Size = new System.Drawing.Size(75, 23);
+            this.BtnMapHotKey.TabIndex = 28;
+            this.BtnMapHotKey.Text = "Назначить";
+            this.BtnMapHotKey.UseVisualStyleBackColor = true;
+            this.BtnMapHotKey.Click += new System.EventHandler(this.BtnMapHotKey_Click);
+            // 
+            // LabelHotKey2
+            // 
+            this.LabelHotKey2.AutoSize = true;
+            this.LabelHotKey2.Location = new System.Drawing.Point(137, 161);
+            this.LabelHotKey2.Name = "LabelHotKey2";
+            this.LabelHotKey2.Size = new System.Drawing.Size(75, 13);
+            this.LabelHotKey2.TabIndex = 29;
+            this.LabelHotKey2.Text = "(HotKeyValue)";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(227, 170);
+            this.ClientSize = new System.Drawing.Size(343, 240);
+            this.Controls.Add(this.LabelHotKey2);
+            this.Controls.Add(this.BtnMapHotKey);
+            this.Controls.Add(this.ComboHotKey);
+            this.Controls.Add(this.LabelHotKey);
             this.Controls.Add(this.BtnStop);
             this.Controls.Add(this.BtnStart);
             this.Controls.Add(this.TextFreq);
@@ -138,7 +214,7 @@
             this.MinimizeBox = false;
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Key Press Emulator";
+            this.Text = "Key Press Emulator 2";
             this.Status.ResumeLayout(false);
             this.Status.PerformLayout();
             this.ResumeLayout(false);
@@ -157,6 +233,12 @@
         private System.Windows.Forms.Label LabelFreq;
         private System.Windows.Forms.TextBox TextKey;
         private System.Windows.Forms.Label LabelKey;
+        private System.Windows.Forms.Label LabelHotKey;
+        private System.Windows.Forms.ComboBox ComboHotKey;
+        private System.Windows.Forms.Button BtnMapHotKey;
+        private System.Windows.Forms.ToolStripStatusLabel StatusEmuState;
+        private System.Windows.Forms.ToolStripStatusLabel StatusPaused;
+        private System.Windows.Forms.Label LabelHotKey2;
     }
 }
 
--- KeyPressEmulator/FormMain.cs
+++ KeyPressEmulator/FormMain.cs
@@ -12,40 +12,102 @@ namespace KeyPressEmulator
 {
     public partial class FormMain : Form
     {
+
+        // конструктор
         public FormMain()
         {
             InitializeComponent();
+            
+            // инициализация элементов интерфейса
+            StatusEmuState.Text = STR_EMU_STOPPED;
+            StatusPaused.Text = STR_EMPTY;
+            StatusCopyright.Text = STR_COPYRIGHT;
+            ComboHotKey.SelectedIndex = 10;
+            LabelHotKey2.Text = STR_EMPTY;
+
+            // инициализация переменных
+            paused = false;
+            hotkey_id = 0;
+            hWnd = Handle;
         }
+
+        #region КОНСТАНТЫ
+
+        const int WM_HOTKEY = 0x0312;
+        const string STR_USER32_DLL = "user32.dll";
+        const string STR_EMU_STOPPED = "Эмуляция остановлена";
+        const string STR_EMU_STARTED = "Эмуляция запущена";
+        const string STR_PAUSED = "Пауза";
+        const string STR_COPYRIGHT = "          © Любимов Роман";
+        const string STR_EMPTY = "";
+        const string STR_ALT_PLUS = "Alt + ";
+        const string STR_CTRL_PLUS = "Ctrl + ";
+        const string STR_SHIFT_PLUS = "Shift + ";
+        const string STR_ERR = "Ошибка!";
+        const string STR_ERR_KEY = "Необходимо задать клавишу или сочетание клавиш";
+        const string STR_ERR_FREQ = "Недопустимое значение частоты";
+        const string STR_ERR_HOTKEY_REM = "Ошибка удаления горячей клавиши";
+        const string STR_ERR_HOTKEY_ADD = "Ошибка установки горячей клавиши";
+
+        #endregion
+        
+        #region ПЕРЕМЕННЫЕ
 
         // сюда запоминаем нажатую клавишу
         byte k;
-        // сюда запоминаем состояние Alt, Ctrl, Shift
+        // сюда запоминаем состояние Alt, Ctrl, Shift при нажатии клавиши
         bool k_alt, k_ctrl, k_shift;
+        // флаг паузы эмуляции
+        bool paused;
+        // идентификатор горячей клавиши паузы
+        int hotkey_id;
+        // дескриптор окна
+        IntPtr hWnd;
 
-        // импорт функции keybd_event для эмуляции нажатия клавиш
-        [DllImport("user32.dll")]
+        #endregion
+
+        #region ИМПОРТ ФУНКЦИЙ
+
+        // импорт функции для эмуляции нажатия клавиш
+        [DllImport(STR_USER32_DLL)]
         public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+        // импорт функции для установки горячей клавиши
+        [DllImport(STR_USER32_DLL)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        
+        // импорт функции для удаления горячей клавиши
+        [DllImport(STR_USER32_DLL)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        
+        #endregion
 
         // срабатывание таймера
         private void Timer_Tick(object sender, EventArgs e)
         {
-            // эмулируем нажатие Alt, Ctrl, Shift
-            if (k_alt)
-                keybd_event((byte)Keys.Menu, 0, 0, 0);
-            if (k_ctrl)
-                keybd_event((byte)Keys.ControlKey, 0, 0, 0);
-            if (k_shift)
-                keybd_event((byte)Keys.ShiftKey, 0, 0, 0);
-            // эмулируем нажатие и отпускание клавиши
-            keybd_event(k, 0, 0, 0);
-            keybd_event(k, 0, 2, 0);
-            // эмулируем отпускание Alt, Ctrl, Shift
-            if (k_alt)
-                keybd_event((byte)Keys.Menu, 0, 2, 0);
-            if (k_ctrl)
-                keybd_event((byte)Keys.ControlKey, 0, 2, 0);
-            if (k_shift)
-                keybd_event((byte)Keys.ShiftKey, 0, 2, 0);
+            // эмуляция выполняется если не пауза
+            if (!paused)
+            {
+                // эмулируем нажатие Alt, Ctrl, Shift
+                if (k_alt)
+                    keybd_event((byte)Keys.Menu, 0, 0, 0);
+                if (k_ctrl)
+                    keybd_event((byte)Keys.ControlKey, 0, 0, 0);
+                if (k_shift)
+                    keybd_event((byte)Keys.ShiftKey, 0, 0, 0);
+               
+                // эмулируем нажатие и отпускание клавиши
+                keybd_event(k, 0, 0, 0);
+                keybd_event(k, 0, 2, 0);
+                
+                // эмулируем отпускание Alt, Ctrl, Shift
+                if (k_alt)
+                    keybd_event((byte)Keys.Menu, 0, 2, 0);
+                if (k_ctrl)
+                    keybd_event((byte)Keys.ControlKey, 0, 2, 0);
+                if (k_shift)
+                    keybd_event((byte)Keys.ShiftKey, 0, 2, 0);
+            }
         }
 
         // нажатие клавиши в поле "Клавиша"
@@ -53,30 +115,35 @@ namespace KeyPressEmulator
         {
             // запрещаем передачу события элементу управления
             e.SuppressKeyPress = true;
+            
             // сбрасываем предыдущую сохраненную комбинацию клавиш
             k = 0;
             k_alt = false;
             k_ctrl = false;
             k_shift = false;
-            TextKey.Text = "";
+            TextKey.Text = STR_EMPTY;
+           
             // проверяем комбинацию Alt + клавиша
             if (e.Alt && (e.KeyCode != Keys.Menu))
             {
                 k_alt = true;
-                TextKey.Text += "Alt + ";
+                TextKey.Text += STR_ALT_PLUS;
             }
+           
             // проверяем комбинацию Ctrl + клавиша
             if (e.Control && (e.KeyCode != Keys.ControlKey))
             {
                 k_ctrl = true;
-                TextKey.Text += "Ctrl + ";
+                TextKey.Text += STR_CTRL_PLUS;
             }
+           
             // проверяем комбинацию Shift + клавиша
             if (e.Shift && (e.KeyCode != Keys.ShiftKey))
             {
                 k_shift = true;
-                TextKey.Text += "Shift + ";
+                TextKey.Text += STR_SHIFT_PLUS;
             }
+          
             // запоминаем нажатую клавишу
             k = (byte)e.KeyCode;
             TextKey.Text += e.KeyCode.ToString();
@@ -95,9 +162,10 @@ namespace KeyPressEmulator
             // проверяем задана ли комбинация клавиш
             if (TextKey.Text == "")
             {
-                MessageBox.Show("Необходимо задать клавишу или сочетание клавиш", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(STR_ERR_KEY, STR_ERR, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+
             // задаем интервал таймера
             try
             {
@@ -105,22 +173,28 @@ namespace KeyPressEmulator
             }
             catch (FormatException)
             {
-                MessageBox.Show("Недопустимое значение частоты", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(STR_ERR_FREQ, STR_ERR, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
             catch (OverflowException)
             {
-                MessageBox.Show("Недопустимое значение частоты", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(STR_ERR_FREQ, STR_ERR, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+
             // запускаем таймер
             Timer.Start();
+
             // блокируем элементы управления для запуска эмуляции
             TextKey.Enabled = false;
             TextFreq.Enabled = false;
             BtnStart.Enabled = false;
+
             // разблокируем элементы управления для остановки эмуляции
             BtnStop.Enabled = true;
+
+            // текст в строке статуса
+            StatusEmuState.Text = STR_EMU_STARTED;
         }
 
         // нажатие на кнопку "Стоп"
@@ -128,12 +202,63 @@ namespace KeyPressEmulator
         {
             // останавливаем таймер
             Timer.Stop();
+
             // блокируем элементы управления для остановки эмуляции
             BtnStop.Enabled = false;
+
             // разблокируем элементы управления для запуска эмуляции
             TextKey.Enabled = true;
             TextFreq.Enabled = true;
             BtnStart.Enabled = true;
+
+            // текст в строке статуса
+            StatusEmuState.Text = STR_EMU_STOPPED;
         }
+        
+        // нажатие на кнопку "Назначить"
+        private void BtnMapHotKey_Click(object sender, EventArgs e)
+        {
+            // если горячая клавиша установлена - удаляем ее
+            if (hotkey_id > 0)
+            {
+                if (UnregisterHotKey(hWnd, hotkey_id))
+                    LabelHotKey2.Text = "";
+                else
+                    MessageBox.Show(STR_ERR_HOTKEY_REM, STR_ERR, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+            // определяем код новой горячей клавиши
+            // коды виртуальных клавиш: F1=70=112 - F12=7B=123
+            // индексы в ComboBox: 0 - 11
+            uint VK = (uint)ComboHotKey.SelectedIndex + 112;
+            
+            // регистрируем новую горячую клавишу
+            hotkey_id = 10;
+            if (RegisterHotKey(hWnd, hotkey_id, 0, VK))
+                LabelHotKey2.Text = "(F" + (VK - 111) + ")";
+            else
+                MessageBox.Show(STR_ERR_HOTKEY_ADD, STR_ERR, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        
+        // переопределение оконной процедуры
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            
+            // если окном получено сообщение WM_HOTKEY
+            // значит нажата горячая клавиша
+            if (m.Msg == WM_HOTKEY)
+            {
+                // переключить флаг паузы
+                paused = !paused;
+
+                // текст в строке статуса
+                if (paused)
+                    StatusPaused.Text = STR_PAUSED;
+                else
+                    StatusPaused.Text = STR_EMPTY;
+            }                           
+        }   
+
     }
 }
